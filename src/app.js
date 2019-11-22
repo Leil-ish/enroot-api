a6606e455e5c2c208bf527ad34d1febb7c65e11e
Leila Anderson <leilacanderson@gmail.com>
2019-11-21 19:49:20 -0600

    added alpha database structure, auth/user router/service

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
-const booksRouter = require('./books/books-router')
-const notesRouter = require('./notes/notes-router')
+const plantsRouter = require('./plants/plants-router')
+const ordersRouter = require('./orders/orders-router')
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 
@@ -19,8 +19,8 @@ app.use(cors())
 
 app.use(helmet())
 
-app.use('/api/garden', booksRouter)
-app.use('/api/orders', notesRouter)
+app.use('/api/garden', plantsRouter)
+app.use('/api/orders', ordersRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
 
