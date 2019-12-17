2b78e81d233b7b26ff618ecd6bdb5da8c78cd9a2
Leila Anderson <leilacanderson@gmail.com>
2019-12-16 21:54:01 -0600

    fixed issues with serializing orders

--- src/trefle/trefle-router.js
+++ src/trefle/trefle-router.js
@@ -2,38 +2,15 @@ const express = require('express')
 const {requireAuth} = require('../middleware/jwt-auth')
 const trefleRouter = express.Router()
 const request =  require('request') 
-const fetch = require('node-fetch');
 
-module.exports = (trefleRouter) => {
+trefleRouter
+    .route('/')
+    .all(requireAuth)
+    request.get(`https://trefle.io/api/species?common_name=dogwood&token=${process.env.BEARER_TOKEN}`, (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        console.dir(JSON.parse(body));
+    })
 
-	let searchTerm;
-
-
-    trefleRouter
-        .route('/')
-        .get((req, res, next) => {
-            const baseUrl = 'https://trefle.io/api/species?common_name=';	
-            const token = `&token=${process.env.BEARER_TOKEN}`;
-
-            const userSearch = (baseUrl, token, searchTerm) => {
-
-            let newUrl = baseUrl + searchTerm + token;
-            return newUrl;
-            };	
-
-            const apiUrl = userSearch(baseUrl, token, searchTerm);
-
-
-		fetch(apiUrl)
-            .then(res => res.json())
-            .then(data => {
-                res.send({ data });
-            })
-            .catch(err => {
-                res.redirect('/error');
-            });
-	})
-
-}
-
-// https://trefle.io/api/species?common_name=dogwood&token=${process.env.BEARER_TOKEN}
+module.exports = trefleRouter
